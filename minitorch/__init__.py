6d13ad369cd033afb1c7f117d93dd15c3097c8da
Jack Yang <catching@bu.edu>
2024-11-16 14:19:34 -0500

    finished 3.1 and 3.2

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,3 +1,27 @@
+"""Initialize the package and expose key classes and functions.
+
+This file serves as the package initializer and handles the importing and exposing of
+key modules, classes, and functions to make them available at the package level.
+
+Imports:
+--------
+- `MathTest`, `MathTestVariable` from `testing` module:
+  Import specific classes for testing mathematical functions.
+
+- All public members from `module`, `testing`, and `datasets` modules:
+  Import all public functions, classes, and variables from these modules to simplify
+  access and usage at the package level.
+
+Notes
+-----
+- `# type: ignore # noqa: F401,F403`:
+  These comments are used to suppress type-checking warnings (`type: ignore`) and flake8
+  warnings:
+  - `F401`: Ignore "imported but unused" warnings.
+  - `F403`: Ignore "from module import *" usage warnings.
+
+"""
+
 from .testing import MathTest, MathTestVariable  # type: ignore # noqa: F401,F403
 from .fast_ops import *  # noqa: F401,F403
 from .cuda_ops import *  # noqa: F401,F403
